4023bdc30ddf38cb4c7c75d224e4b95a6c2ad2fc
A. Matías Quezada <amatiasq@gmail.com>
2024-10-28 12:45:33 +0100

    Fullstack scaffold

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import devServer from '@hono/vite-dev-server';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 4000, // change to a custom port
+  },
+  build: {
+    outDir: 'build', // change to 'build', explain later
+  },
+  plugins: [
+    react(),
+    devServer({
+      entry: 'src/server.ts',
+      exclude: [
+        // We need to override this option since the default setting doesn't fit
+        /.*\.tsx?($|\?)/,
+        /.*\.(s?css|less)($|\?)/,
+        /.*\.(svg|png)($|\?)/,
+        /^\/@.+$/,
+        /^\/favicon\.ico$/,
+        /^\/(public|assets|static)\/.+/,
+        /^\/node_modules\/.*/,
+      ],
+      injectClientScript: false, // This option is buggy, disable it and inject the code manually
+    }),
+  ],
+});
